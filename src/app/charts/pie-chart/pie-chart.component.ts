3b270e1547e26b0b3efbceeb81994f562543276d
Matyi <matyas.csorba@melkweg.hu>
2020-07-02 15:09:06 +0200

    pie chart added

--- src/app/charts/pie-chart/pie-chart.component.ts
+++ src/app/charts/pie-chart/pie-chart.component.ts
@@ -9,6 +9,16 @@ export class PieChartComponent implements OnInit {
 
   constructor() { }
 
+  public pieChartData: number[] = [350, 450, 120];
+  public pieChartLabels: string[] = ['XYZ', 'ABC', 'ERT'];
+  public colors: any[] = [
+    {
+      backgroundColor: ['#26547c', '#ff6b6b', '#ffd166'],
+      borderColor: '#111'
+    }
+  ];
+  public pieChartType = 'doughnut';
+
   ngOnInit() {
   }
 
