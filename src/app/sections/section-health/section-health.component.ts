d6718ccbee28b1e7677888a0a3eafaad1b07b5e4
Matyi <matyas.csorba@melkweg.hu>
2020-07-02 17:21:47 +0200

    health page started, refactor on components

--- src/app/sections/section-health/section-health.component.ts
+++ src/app/sections/section-health/section-health.component.ts
@@ -1,4 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {Server} from '../../shared/server';
+
+const SAMPLE_SERVERS: Server[] = [
+  {
+    id: 1,
+    name: 'server-1',
+    isOnline: true,
+  },
+  {
+    id: 2,
+    name: 'server-2',
+    isOnline: false,
+  },
+  {
+    id: 3,
+    name: 'server-3',
+    isOnline: true,
+  },
+];
 
 @Component({
   selector: 'app-section-health',
@@ -8,7 +27,7 @@ import { Component, OnInit } from '@angular/core';
 export class SectionHealthComponent implements OnInit {
 
   constructor() { }
-
+  public servers: Server[] = SAMPLE_SERVERS;
   ngOnInit() {
   }
 
